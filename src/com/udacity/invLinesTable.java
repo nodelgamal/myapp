b60fbd298834a8de30d3d4a31e110747696fa65a
unknown <gogaahmed222@gmail.com>
2022-04-24 05:35:24 +0200

    first commit

---
+++ src/com/udacity/invLinesTable.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udacity;
+
+import com.Invoice.model.InvoiceLinesTableModel;
+
+/**
+ *
+ * @author Taroook-3ashry
+ */
+class invLinesTable {
+
+    static void setModel(InvoiceLinesTableModel invoiceLinesTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getSelectedRow() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/com/udacity/invoicesTable.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udacity;
+
+import com.Invoice.model.InvoiceHeaderTableModel;
+
+/**
+ *
+ * @author Taroook-3ashry
+ */
+class invoicesTable {
+
+    static void validate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setModel(InvoiceHeaderTableModel invoiceHeaderTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getSelectedRow() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
